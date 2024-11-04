2cae6e98bcb240d3245e865b40bfb25899edcef9
sidpd51 <sidpd510@gmail.com>
2024-11-04 23:28:41 +0530

    feat: sequelize setup and add ticket entity

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const express = require('express')
 
 const setupAndstartServer=()=>{
     const app = express()
+
     app.use(express.json())
     app.use(express.urlencoded({extended:true}))
+    
     app.listen(PORT,()=>{
         console.log(`Server is running on port no: ${PORT}`)
     })
